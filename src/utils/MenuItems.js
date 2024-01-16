629a60acfb184efcf77aa7563b40b43a45fa6765
hadiRazal <hadhirasal22@gmail.com>
2024-01-16 21:13:09 +0530

    small update in menu

--- src/utils/MenuItems.js
+++ src/utils/MenuItems.js
@@ -1,12 +1,12 @@
 const MenuItems = [
-    { name: "Green Tea", type: "Beverage" },
-    { name: "Black Coffee", type: "Beverage" },
-    { name: "Hot and Sour Soup", type: "Soup" },
-    { name: "Mushroom Soup", type: "Soup" },
+    { name: "Green Tea", type: "Beverages" },
+    { name: "Black Coffee", type: "Beverages" },
+    { name: "Hot and Sour Soup", type: "Soups" },
+    { name: "Mushroom Soup", type: "Soups" },
     { name: "Chicken Momos", type: "Momos", preparation: "Steamed" },
     { name: "Vegetable Momos", type: "Momos", preparation: "Fried" },
-    { name: "Chai", type: "Beverage" },
-    { name: "Tomato Soup", type: "Soup" },
+    { name: "Chai", type: "Beverages" },
+    { name: "Tomato Soup", type: "Soups" },
     { name: "Pork Momos", type: "Momos", preparation: "Fried" },
 ];
 
