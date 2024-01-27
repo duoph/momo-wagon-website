e601b912b6558b5fb603b3064bcd1ce2ec82b756
Praveen Prasad <praveenprasad2510@gmail.com>
2024-01-27 23:32:30 +0530

    Added More Testimonials

--- src/utils/Testimonial.js
+++ src/utils/Testimonial.js
@@ -1,4 +1,5 @@
 const customerReview = [
+  // Review 1
   {
     id: 1,
 
@@ -7,6 +8,81 @@ const customerReview = [
       "A taste of heaven in Kottakal! This cafe's momos and soups are a treat for your taste buds. The ambiance is like stepping into an art gallery, making it the perfect place to savor delicious food and enjoy a lovely atmosphere.",
     image:
       "https://lh3.googleusercontent.com/a-/ALV-UjWWBjiGE11az_bfq--UjOo3aJySCa-JcU3e9wqFqMmyHw=w75-h75-p-rp-mo-br100",
+  },
+  // Review 2
+  {
+    id: 1,
+    
+    name: "Shabeeb Shaz",
+    review:
+    "Absolutely the best momos in town! The artistic flair in the restaurant's design adds to the overall cozy ambiance, making it a great spot to enjoy a meal and chill.",
+    image:
+    "https://lh3.googleusercontent.com/a-/ALV-UjXAP_CcpoZWw_57mOwi7dildtKtWx5enQyo91J_31ESmw=w75-h75-p-rp-mo-br100",
+  },
+  // Review 3
+  {
+    id: 1,
+    
+    name: "Labi Labiez",
+    review:
+    "If you're a momo lover like me, this cafe in Kottakal is a must-visit. The momos are incredibly flavorful and authentic, and their selection of soups complements them perfectly. Plus, the artistic decor adds a unique touch to the dining experience.",
+    image:
+    "https://lh3.googleusercontent.com/a-/ALV-UjXfDaboyGsI9CGzZ4YjDDTVKPvQeC1B81VpZlnGdRfXV64=w75-h75-p-rp-mo-br100",
+  },
+
+  // Review 4
+  {
+    id: 1,
+    
+    name: "Shifana Shifu",
+    review:
+    "I love how this cafe seamlessly blends the world of art, delicious momos, and soothing soups. The ambiance is incredibly artistic, and the peaceful atmosphere makes it an ideal destination for those seeking both great food and a serene dining experience.",
+    image:
+    "https://lh3.googleusercontent.com/a-/ALV-UjUx-C2ngZYegv2XhfWlMMolSkgTApfzMccsERMyuT5RRw=w75-h75-p-rp-mo-br100",
+  },
+
+  // Review 5
+  {
+    id: 1,
+    
+    name: "SAS PC",
+    review:
+    "Tried almost all Flavours they have. All of them are delicious. Also excellent service and fantastic ambience",
+    image:
+    "https://lh3.googleusercontent.com/a-/ALV-UjVOq8f5LiyBLvIFfuwjdIFqFPv-F_fH-mANcSkFHm9l8Cs=w75-h75-p-rp-mo-br100",
+  },
+
+  // Review 6
+  {
+    id: 1,
+    
+    name: "Muneesha Imthiyaz",
+    review:
+    "The ambiance is welcoming, and the momos are simply exceptional – a must-visit for momo enthusiasts! MomoWagon 🥰",
+    image:
+    "https://lh3.googleusercontent.com/a/ACg8ocLALikq_WQJ2G7tYe763KvYpJFxVVqMoktkRf8nZfiB=w75-h75-p-rp-mo-br100",
+  },
+
+  // Review 7
+  {
+    id: 1,
+    
+    name: "Femina Jafar",
+    review:
+    "Night vibe cafe... Darjeeling tea and fried momos best combo ever.. That music and the ambience like a addiction it's giving much positivity...",
+    image:
+    "https://lh3.googleusercontent.com/a/ACg8ocJ79NnkW_lJITRRbSJSh5LNLnrv8wmXcS2WteMpbpXk=w75-h75-p-rp-mo-br100",
+  },
+
+  // Review 8
+  {
+    id: 1,
+
+    name: "Hazna Hamza",
+    review:
+    "Vazharu kedaavathe vishwasich kazhikkan pattiyya place … i’m addicted to their momos ..best ever i had",
+    image:
+      "https://lh3.googleusercontent.com/a-/ALV-UjUifRHnzQCp1z4j5Fb1lneZfPuqWy4goi8pWObiqb0nM4be=w75-h75-p-rp-mo-br100",
   },
 ];
 
